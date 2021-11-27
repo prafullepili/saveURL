765d8ff746917eb3e3f934e742b1e0001b934073
prafull epili <prafullepili@gmail.com>
2021-11-28 01:23:43 +0530

    updating

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let myLeads = []
+Password = "1615"
+let saveURLpublic = []
+let saveURLprivate = []
+let passCheck = false
+
 const inputEl = document.getElementById("input-el") //url input
 const urlForm = document.getElementById("url-form") //url input
 const ulEl = document.getElementById("ul-el") //url list
@@ -6,34 +10,29 @@ const ulEl = document.getElementById("ul-el") //url list
 const inputBtn = document.getElementById("input-btn") // save from input
 const tabBtn = document.getElementById("tab-btn") // save from current tab
 const deleteBtn = document.getElementById("delete-btn") //delete all
+const privateCbox = document.getElementById("private") //private checkbox
 
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) //Convert str to array
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}else{
-    noLeads(myLeads)
+function loadLocalStorage() {
+    if (passCheck){
+       saveURLprivate =  JSON.parse(localStorage.getItem("urlPrivate")) || []
+       render(saveURLprivate)
+    }
+    else{
+        saveURLpublic =  JSON.parse(localStorage.getItem("urlPublic")) || []
+        render(saveURLpublic)
+    }
 }
-
-//save from current tab
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url);
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) );
-        render(myLeads);
-    })
-})
+loadLocalStorage()
 
 //render all url from localStorage to ul
-function render(leads) {
+function render(urlsArray) {
     let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
+    for (let i = 0; i < urlsArray.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
+                <a target='_blank' href='${urlsArray[i]}'>
+                    ${urlsArray[i]}
                 </a>
             </li>
         `
@@ -41,31 +40,120 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
+// If no url found
 function noLeads(leads){
     if (leads){
         ulEl.innerHTML = "<li class='notFound'>No save URL</li>"
     }
 }
-//delete all record
-deleteBtn.addEventListener("click", function() {
-    if (myLeads.length != 0){
-        let Yes = confirm("Do you want to delete all?");
+
+// Delete All fucniotn 
+function deleteAll(storageArray,key) {
+    console.log(storageArray)
+    if (storageArray.length != 0){
+        let Yes = confirm(`Do you want to delete all ${passCheck ? 'Private' : 'Public'} urls?`);
         if (Yes){
-            console.log(localStorage.removeItem("myLeads"))
-            myLeads = []
-            render(myLeads)
-            noLeads(myLeads);
+            storageArray = []
+            localStorage.removeItem(key);
+            render(storageArray)
+            noLeads(storageArray);
         }
     }
-})
+}
+
+// save url from input box
+function saveUrl(storageArray,key) {
+    storageArray.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem(key, JSON.stringify(storageArray))
+    render(storageArray)
+}
+
+//save form current tab
+function saveUrlFromTab(storageArray,key) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        storageArray.push(tabs[0].url);
+        localStorage.setItem(key, JSON.stringify(saveURLpublic) );
+        render(storageArray);
+    })
+}
+
+// On click private this will set password
+function setPassword(pass){ 
+    let password = CryptoJS.MD5(pass).toString();
+    localStorage.setItem('upass',password);
+}
+
+// Check password
+function checkPassword(pass){
+    passFromLocalStorage = localStorage.getItem("upass")
+    if (passFromLocalStorage === CryptoJS.MD5(pass).toString()){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+
+
 
 //save from input box
 urlForm.addEventListener("submit", function(e) {
     e.preventDefault();
     if (inputEl.value != ""){
-        myLeads.push(inputEl.value)
-        inputEl.value = ""
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
+        if (passCheck){
+            saveUrl(saveURLprivate,"urlPrivate")
+        }else{
+            saveUrl(saveURLpublic,"urlPublic")
+        }
+    }
+})
+
+//save from current tab
+tabBtn.addEventListener("click", function(){
+    if (passCheck){
+        saveUrlFromTab(saveURLprivate,"urlPrivate")
+    }else{
+        saveUrlFromTab(saveURLpublic,"urlPublic")
+    }
+})
+
+
+//change mode public to private
+privateCbox.addEventListener("change",()=>{
+    if (privateCbox.checked){
+        setPassword(Password);
+        let getPass = prompt("Enter your password: ");
+        if (!getPass){
+            privateCbox.checked = false;
+        }else{
+            passCheck = checkPassword(getPass);
+            if (passCheck){
+                loadLocalStorage()
+                render(saveURLprivate)
+            }else{
+                privateCbox.checked = false;
+                alert("Your password is wrong!");
+                passCheck = false;
+                loadLocalStorage()
+                render(saveURLpublic)
+            }
+        }
+    }else { 
+        passCheck = false;
+        loadLocalStorage()
+        render(saveURLpublic)
+    }
+})
+
+//delete all record
+deleteBtn.addEventListener("click", function() {
+    if (passCheck){    
+        loadLocalStorage()
+        deleteAll(saveURLprivate,"urlPrivate")
+    }else{
+        loadLocalStorage()
+        deleteAll(saveURLpublic,"urlPublic")
     }
 })
